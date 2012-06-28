d423d4974046177fc3be3d476033652e0f219610
Michael Hartman <mphartman@me.com>
2012-06-27 22:13:19 -0500

    added digit keys

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -9,11 +9,24 @@
 #import "CalculatorViewController.h"
 
 @interface CalculatorViewController ()
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
 @end
 
 @implementation CalculatorViewController
 
 @synthesize display = _display;
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+
+- (IBAction)digitPressed:(UIButton *)sender 
+{
+    NSString *digit = sender.currentTitle;
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+    self.display.text = [self.display.text stringByAppendingString:digit];
+    } else {
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
+}
 
 - (void)viewDidUnload 
 {
